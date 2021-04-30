30aed32975ae381166ea5b8a938ce0ed5e7cb798
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-04-30 09:35:28 +0000

    add log

--- src/main/java/com/mycompany/app/HelloWorldHandler.java
+++ src/main/java/com/mycompany/app/HelloWorldHandler.java
@@ -1,19 +1,31 @@
 package com.mycompany.app;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class HelloWorldHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
     @Override
     public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        LambdaLogger logger = context.getLogger();
         Map<String, Object> output = new HashMap<>();
         output.put("greeting", "Hello, lambda world");
         output.put("input", input);
         output.put("context", context);
+        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+        logger.log("CONTEXT: " + gson.toJson(context));
+        // process event
+        logger.log("EVENT: " + gson.toJson(input));
+        logger.log("EVENT TYPE: " + input.getClass());
         return output;
     }
 }
